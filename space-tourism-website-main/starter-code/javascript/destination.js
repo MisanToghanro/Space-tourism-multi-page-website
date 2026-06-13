9be49ddfb204bc1f372a851de2cdf401be648e44
MisanToghanro <tmisan657@gmail.com>
2026-06-13 12:13:16 +0100

    Refactor navigation into shared layout, and built  dynamic destination page with JSON data, and refactor navigation system

---
+++ space-tourism-website-main/starter-code/javascript/destination.js
@@ -0,0 +1,41 @@
+
+const planet = document.querySelector("#destination-name");
+const image = document.querySelector("#destination-image");
+const description = document.querySelector("#destination-description");
+const distance = document.querySelector("#destination-distance");
+const travel = document.querySelector("#destination-travel");
+const tabs = document.querySelectorAll(".destination-tabs button")
+
+
+const loadDestinations = async (params) => {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+
+
+    renderDestination(data.destinations[0]);
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const planetName = tab.dataset.planet;
+            const destination = data.destinations.find(
+                item => item.name === planetName
+            );
+            renderDestination(destination)
+        })
+    })
+    
+}
+
+loadDestinations();
+
+const renderDestination = (destination) => {
+
+    image.src = destination.images.png;
+    image.alt = destination.name;
+
+    planet.textContent = destination.name;
+    description.textContent = destination.description;
+    distance.textContent = destination.distance;
+    travel.textContent = destination.travel
+
+}
--- space-tourism-website-main/starter-code/javascript/index.js
+++ space-tourism-website-main/starter-code/javascript/index.js
@@ -1,11 +1,21 @@
 
 const menuButton = document.querySelector(".menu-btn");
 const navMenu = document.querySelector(".menu");
+const menuIcon = document.querySelector("#menu-icon")
 
 
-if(menuButton && navMenu) {
+if(menuButton && navMenu && menuIcon) {
     menuButton.addEventListener("click" , () => {
               navMenu.classList.toggle("open");
+
+              if(navMenu.classList.contains("open")) {
+                 menuIcon.src = "./assets/shared/icon-close.svg";
+                 menuIcon.alt = "Close menu";
+                 
+              }else {
+                 menuIcon.src = "./assets/shared/icon-hamburger.svg";
+                 menuIcon.alt = "Open menu"
+              }
 
     })
 }
